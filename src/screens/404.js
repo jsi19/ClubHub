ba6305bcc9d036836963ab12f9ce32b3c64df4b5
LeothePold <leo.naddell@gmail.com>
2023-05-17 22:56:08 -0700

    error changes
    r Please enter the commit message for your changes. Lines star

--- src/screens/404.js
+++ src/screens/404.js
@@ -2,8 +2,9 @@ import React from "react";
 
 export default function NotFound() {
   return (
-    <div className="flex h-screen">
-      <h1 className="text-4xl m-auto">Page Not Found</h1>
+    <div className="flex h-screen bg-gray-200">
+      <h1 className="text-4xl m-auto text-red-500">Oops! Page Not Found</h1>
+      <p className="text-lg m-auto">The page you are looking for does not exist.</p>
     </div>
   );
 }
