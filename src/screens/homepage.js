a41fa64d28809d7b8258e415433c475d0c5d5835
LeothePold <leo.naddell@gmail.com>
2023-05-17 22:59:06 -0700

    homepage changes

--- src/screens/homepage.js
+++ src/screens/homepage.js
@@ -1,22 +1,30 @@
 import { motion } from "framer-motion";
 import React from "react";
 
-export default function Home() {
+const Home = () => {
+  const animationVariants = {
+    initial: { opacity: 0, y: -400 },
+    animate: {
+      scale: 1.5,
+      opacity: 1,
+      y: 0,
+      transition: { duration: 1 },
+      rotate: 360,
+    },
+  };
+
   return (
     <div className="flex h-screen">
       <motion.h1
-        // initial={{ opacity: 0, y: -400 }}
-        // animate={{
-        //   scale: 1.5,
-        //   opacity: 1,
-        //   y: 0,
-        //   transition: { duration: 1 },
-        //   rotate: 360,
-        // }}
+        initial="initial"
+        animate="animate"
+        variants={animationVariants}
         className="m-auto text-3xl"
       >
         Welcome To ClubHub!
       </motion.h1>
     </div>
   );
-}
+};
+
+export default Home;
