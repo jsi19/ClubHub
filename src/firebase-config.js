af2df3a7350a216ec48aebd955ac0d1e20e586e7
jsi19 <jonathan.si0619@gmail.com>
2023-05-30 16:33:48 -0700

    Integrated firebase authentication with signup/login functionality

---
+++ src/firebase-config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCkrafwTirf4tDBD7AnaiDGwSo2ToaaKH0",
+  authDomain: "clubhub-36763.firebaseapp.com",
+  projectId: "clubhub-36763",
+  storageBucket: "clubhub-36763.appspot.com",
+  messagingSenderId: "513577731707",
+  appId: "1:513577731707:web:9939c76e3aa56daa0304de",
+  measurementId: "G-D3M3GYJWWR"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth(app);
