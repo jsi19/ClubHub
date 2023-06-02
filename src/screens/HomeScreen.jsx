ee8cbba5a01d13feb9fe519214766968e84b7f2d
emilyfaj <emilyfajardo@g.ucla.edu>
2023-06-02 15:18:29 -0700

    poldy3

--- src/screens/HomeScreen.jsx
+++ src/screens/HomeScreen.jsx
@@ -4,6 +4,7 @@ import './HomeScreen.css';
 import { collection, addDoc, getDocs, query, where } from 'firebase/firestore';
 import { getAuth } from 'firebase/auth';
 import { firestore } from '../firebase-config';
+import { Link } from 'react-router-dom';
 
 // Initialize Firebase
 const auth = getAuth();
@@ -126,6 +127,9 @@ const HomeScreen = () => {
                 ) : (
                   <button onClick={() => handleAddClub(club.id)}>Add Club</button>
                 )}
+                <Link to={`/club-profile/${club.id}`}>
+                       <button style={ClubProfileButton}>Club Profile</button>
+                </Link>
               </div>
             </div>
           ))}
@@ -133,6 +137,19 @@ const HomeScreen = () => {
       </section>
     </div>
   );
+};
+
+const ClubProfileButton = {
+  fontFamily: 'Poppins, sans-serif',
+  backgroundColor: '#115D81',
+  fontWeight: 'bold',
+  fontSize: '16px',
+  alignItems: 'center',
+  color: 'white',
+  margin: '10px 2px',
+  padding: '20px 50px',
+  display: 'inline-block',
+  border: 'none',
 };
 
 export default HomeScreen;
